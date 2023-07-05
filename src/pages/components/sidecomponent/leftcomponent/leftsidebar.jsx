23c4b673bb60cc248676fb247496144e3a7161d7
OpenSourcePundit <shashank00098@gmail.com>
2023-07-06 00:19:20 +0530

    basic changes

--- src/pages/components/sidecomponent/leftcomponent/leftsidebar.jsx
+++ src/pages/components/sidecomponent/leftcomponent/leftsidebar.jsx
@@ -41,6 +41,59 @@ export const LeftSideBar = () =>{
             <button style={{borderRadius:"15px"}} className="mt-m p-s primary-bg  white-color border-none outline-transparent new-post-btn" onClick={()=>navigate('/home')}>
               Create New Post
             </button>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
+
+ {/* Modal */}
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+    <div className="white-bg  p-xs mt-s">
+          <div className="flex flex-row nowrap p-xs">
+            <div
+              className="grey-bg br-full width-xl height-xl p-xs mr-xs create-box-left"style={{ aspectRatio: 1 }}>
+              </div>
+            <div className="w-full">
+              <textarea
+                name=""
+                id=""
+                cols="70"
+                rows="6"
+                className="w-full lynx-white-bg p-s outline-transparent border-none"
+                style={{ resize: "none" }}
+                placeholder="Write something interesting..."
+              ></textarea>
+              {/* <div className="flex flex-space-between pt-s">
+                <div className="flex" style={{ gap: "1rem" }}>
+                  <i className="bi bi-card-image gen-btn"></i>
+                  <i className="bi bi-filetype-gif gen-btn"></i>
+                  <i className="bi bi-emoji-smile gen-btn"></i>
+                </div>
+                <button className="primary-bg p-l pt-xs pb-xs secondary-color border-none outline-transparent">
+                  Post
+                </button>
+              </div> */}
+            </div>
+          </div>
+        </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary primary primary-bg">Post</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
           </div>
       <div className="flex flex-space-between white-bg  username-box">
         <div className="grey-bg br-full width-xs height-xs   post-box-left" style={{ aspectRatio: 1 }}></div>
