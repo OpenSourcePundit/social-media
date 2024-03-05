478a81d3fbada8466fd405b74c6b29709265ac26
Shashank Kumar <shashank00098@gmail.com>
2024-03-05 15:20:00 +0530

    fix: Landingpage fixed for responsiveness

--- src/pages/landingpage/landingpage.jsx
+++ src/pages/landingpage/landingpage.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import "./../../base.css";
 import "./../../utility.css";
-
+import "./landingpage.css"
 import {useNavigate} from 'react-router-dom'
-
 
 
 
@@ -11,7 +10,7 @@ export function LandingPage() {
   const navigate = useNavigate();
   return (
     <div className="flex flex-row flex-center h-full w-full" style={{ gap: '10rem' }}>
-      <div className="flex flex-column flex-space-between" style={{ height: '70vh', width: '25rem' }}>
+      <div className="flex flex-column flex-space-between social-circle-landing-text" style={{ height: '70vh', width: '25rem' }}>
         <h2 className="fw-black txt-xl"><span className="primary-color">Social</span> Circle</h2>
         <div className="flex flex-column" style={{ gap: '1.2rem' }}>
           <h2 className="txt-xs">
@@ -25,11 +24,12 @@ export function LandingPage() {
           </h2>
         </div>
         <div>
-          <button className="primary-bg w-full p-s secondary-color border-none outline-none txt-s" onClick={()=>navigate("/signup")}>Join Now</button>
+          <button className="primary-bg w-100 p-s secondary-color border-none outline-none txt-s" onClick={()=>navigate("/signup")}>Join Now</button>
           <p className="primary-color txt-s txt-center mt-xs" style={{cursor:"pointer"}} onClick={()=>navigate("/login")}>Already have an account?</p>
         </div>
       </div>
-      <div className="grey-bg" style={{ height: '70vh', width: '30rem' }}></div>
+      <div className="grey-bg social-circle-landing-image" style={{ height: '70vh', width: '30rem' }}>
+      </div>
     </div>
   );
 }
